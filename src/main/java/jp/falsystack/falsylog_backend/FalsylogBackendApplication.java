0af5512fcd611948a9e51abb3e2c568615ae945a
falsystack <falsystack@gmail.com>
2023-10-11 23:13:23 +0900

    first commit

---
+++ src/main/java/jp/falsystack/falsylog_backend/FalsylogBackendApplication.java
@@ -0,0 +1,13 @@
+package jp.falsystack.falsylog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalsylogBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FalsylogBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/jp/falsystack/falsylog_backend/FalsylogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.falsystack.falsylog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalsylogBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
