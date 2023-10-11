540ce22db3c28c0e8bd002f32b4b93299d8ff8b5
falsystack <falsystack@gmail.com>
2023-10-11 23:29:38 +0900

    test: create post test

---
+++ src/test/java/jp/falsystack/falsylog_backend/controller/PostControllerTest.java
@@ -0,0 +1,46 @@
+package jp.falsystack.falsylog_backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import jp.falsystack.falsylog_backend.request.PostCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+public class PostControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Test
+  @DisplayName("ブログ記事登録")
+  void post() throws Exception {
+    // given
+    PostCreate request = PostCreate.builder()
+        .title("記事タイトル")
+        .content("コンテンツ")
+        .author("falsystack")
+        .createdAt(LocalDate.now())
+        .build();
+    String json = objectMapper.writeValueAsString(request);
+
+    // expected
+    mockMvc.perform(MockMvcRequestBuilders.post("/post")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andDo(MockMvcResultHandlers.print());
+  }
+
+
+}
