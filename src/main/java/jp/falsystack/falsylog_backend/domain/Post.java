5ce14f1092022897c7a08a7649f078c63be01bf1
falsystack <falsystack@gmail.com>
2023-10-15 21:24:28 +0900

    feat: post entity

---
+++ src/main/java/jp/falsystack/falsylog_backend/domain/Post.java
@@ -0,0 +1,37 @@
+package jp.falsystack.falsylog_backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jp.falsystack.falsylog_backend.service.dto.PostWrite;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String content;
+  private String author;
+
+  @Builder
+  private Post(String title, String content, String author) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  public static Post from(PostWrite postWrite) {
+    return Post.builder()
+        .title(postWrite.getTitle())
+        .content(postWrite.getContent())
+        .author(postWrite.getAuthor())
+        .build();
+  }
+}
