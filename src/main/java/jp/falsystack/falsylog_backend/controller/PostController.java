8de2096d83a97df2f4a5647adcfb156b52c238a7
falsystack <falsystack@gmail.com>
2023-10-21 00:33:14 +0900

    feat: implements posts controller

--- src/main/java/jp/falsystack/falsylog_backend/controller/PostController.java
+++ src/main/java/jp/falsystack/falsylog_backend/controller/PostController.java
@@ -1,14 +1,15 @@
 package jp.falsystack.falsylog_backend.controller;
 
+import java.util.List;
 import jp.falsystack.falsylog_backend.request.PostCreate;
+import jp.falsystack.falsylog_backend.response.PostResponse;
 import jp.falsystack.falsylog_backend.service.PostService;
 import jp.falsystack.falsylog_backend.service.dto.PostWrite;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -18,11 +19,15 @@ public class PostController {
 
   private final PostService postService;
 
-  @ResponseStatus(HttpStatus.OK)
   @PostMapping("/post")
   public void post(@RequestBody PostCreate postCreate) {
     PostWrite postWrite = PostWrite.from(postCreate);
     postService.write(postWrite);
+  }
+
+  @GetMapping("/posts")
+  public List<PostResponse> posts() {
+    return postService.getPosts();
   }
 
 }
