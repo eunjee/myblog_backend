4df89feceb410e21540198121f97b85a8b95f66d
falsystack <falsystack@gmail.com>
2023-10-21 00:32:40 +0900

    test: create getPosts test

--- src/test/java/jp/falsystack/falsylog_backend/controller/PostControllerTest.java
+++ src/test/java/jp/falsystack/falsylog_backend/controller/PostControllerTest.java
@@ -1,7 +1,12 @@
 package jp.falsystack.falsylog_backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import jp.falsystack.falsylog_backend.domain.Post;
+import jp.falsystack.falsylog_backend.repository.PostRepository;
 import jp.falsystack.falsylog_backend.request.PostCreate;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,19 +24,37 @@ public class PostControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
-
   @Autowired
   private ObjectMapper objectMapper;
+  @Autowired
+  private PostRepository postRepository;
+
+  @BeforeEach
+  void beforeEach() {
+    postRepository.deleteAllInBatch();
+  }
+
+  private static Post createPostDto(int count) {
+    return Post.builder()
+        .title("記事タイトル" + count)
+        .content("コンテンツ" + count)
+        .author("falsystack" + count)
+        .build();
+  }
+
+  private static PostCreate createPostRequestDto(int count) {
+    return PostCreate.builder()
+        .title("記事タイトル" + count)
+        .content("コンテンツ" + count)
+        .author("falsystack" + count)
+        .build();
+  }
 
   @Test
   @DisplayName("ブログ記事登録")
   void post() throws Exception {
     // given
-    PostCreate request = PostCreate.builder()
-        .title("記事タイトル")
-        .content("コンテンツ")
-        .author("falsystack")
-        .build();
+    var request = createPostRequestDto(0);
     String json = objectMapper.writeValueAsString(request);
 
     // expected
@@ -40,6 +63,29 @@ public class PostControllerTest {
             .content(json))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @DisplayName("GET /postsに要請するとメインページの記事一覧を返す")
+  void getPosts() throws Exception {
+    // given
+    var postDto1 = createPostDto(1);
+    var postDto2 = createPostDto(2);
+    var postDto3 = createPostDto(3);
+    postRepository.saveAll(List.of(postDto1, postDto2, postDto3));
+
+    // expected
+    mockMvc.perform(MockMvcRequestBuilders.get("/posts")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.is(3)))
+        .andExpectAll(
+            MockMvcResultMatchers.jsonPath("$[0].title", Matchers.is("記事タイトル1")),
+            MockMvcResultMatchers.jsonPath("$[0].content", Matchers.is("コンテンツ1")),
+            MockMvcResultMatchers.jsonPath("$[0].author", Matchers.is("falsystack1"))
+        )
+        .andDo(MockMvcResultHandlers.print());
+
   }
 
 
