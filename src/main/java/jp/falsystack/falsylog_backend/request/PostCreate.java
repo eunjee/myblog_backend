d4effa696a54c93afc65a20f3b3b6eb415a057e5
falsystack <falsystack@gmail.com>
2023-10-27 22:07:20 +0900

    feat: 記事生成時のValidation追加

--- src/main/java/jp/falsystack/falsylog_backend/request/PostCreate.java
+++ src/main/java/jp/falsystack/falsylog_backend/request/PostCreate.java
@@ -1,12 +1,20 @@
 package jp.falsystack.falsylog_backend.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class PostCreate {
 
+  @NotEmpty(message = "タイトルを入力してください。")
+  @Size(min = 1, max = 20, message = "タイトルは１文字以上２０文字以下で作成してください。")
   private final String title;
+
+  @NotBlank(message = "内容を入力してください。")
+  @Size(min = 10, message = "１０文字以上作成してください。")
   private final String content;
   private final String author;
 
