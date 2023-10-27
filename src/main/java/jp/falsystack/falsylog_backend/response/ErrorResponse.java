2b160aabf5f3db0119700c40d85e7de17ca90ed0
falsystack <falsystack@gmail.com>
2023-10-27 22:06:19 +0900

    feat: ErrorResponse生成

---
+++ src/main/java/jp/falsystack/falsylog_backend/response/ErrorResponse.java
@@ -0,0 +1,39 @@
+package jp.falsystack.falsylog_backend.response;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+/**
+ * {
+ *  "code": "400",
+ *  "message": "잘못된 요청입니다.",
+ *  "validation" : {
+ *    "title": "1글자 입력해주세요."
+ *    }
+ *  }
+ */
+
+@Getter
+public class ErrorResponse {
+
+  private final String code;
+  private final String message;
+  private final Map<String, String> validation = new HashMap<>();
+
+  public void addValidation(List<FieldError> fieldErrors) {
+    for (FieldError fieldError : fieldErrors) {
+      this.validation.put(fieldError.getField(), fieldError.getDefaultMessage());
+    }
+  }
+
+  @Builder
+  public ErrorResponse(String code, String message) {
+    this.code = code;
+    this.message = message;
+  }
+}
