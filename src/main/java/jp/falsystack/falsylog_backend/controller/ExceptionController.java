67ec94aabd416ebf8b5579925201d8b3ead3379f
falsystack <falsystack@gmail.com>
2023-10-27 22:08:13 +0900

    feat: 例外ハンドラー作成

---
+++ src/main/java/jp/falsystack/falsylog_backend/controller/ExceptionController.java
@@ -0,0 +1,26 @@
+package jp.falsystack.falsylog_backend.controller;
+
+import jp.falsystack.falsylog_backend.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+
+    var errorResponse = ErrorResponse.builder()
+        .code("400")
+        .message("間違ったリクエストです。")
+        .build();
+
+    errorResponse.addValidation(e.getFieldErrors());
+
+    return errorResponse;
+  }
+}
