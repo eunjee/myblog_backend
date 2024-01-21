e3392d3fe062c9cf59c21e0dc30ecee68ed0fbe5
eunjee <rira9817@gmail.com>
2024-01-21 23:12:15 +0900

    refactor: 쿼리 내에서 해시태그 검색하도록 수정

--- src/main/java/jp/falsystack/falsylog_backend/FalsylogBackendApplication.java
+++ src/main/java/jp/falsystack/falsylog_backend/FalsylogBackendApplication.java
@@ -2,9 +2,15 @@ package jp.falsystack.falsylog_backend;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import jp.falsystack.falsylog_backend.domain.HashTag;
 import jp.falsystack.falsylog_backend.domain.Member;
 import jp.falsystack.falsylog_backend.domain.Post;
+import jp.falsystack.falsylog_backend.domain.PostHashTag;
+import jp.falsystack.falsylog_backend.repository.HashTagRepository;
 import jp.falsystack.falsylog_backend.repository.MemberRepository;
+import jp.falsystack.falsylog_backend.repository.PostHashTagRepository;
 import jp.falsystack.falsylog_backend.repository.post.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
@@ -29,9 +35,9 @@ public class FalsylogBackendApplication {
   @Bean
   @Profile("local")
   public TestDataInit testDataInit(MemberRepository memberRepository,
-      PostRepository postRepository) {
+                                   PostRepository postRepository, PostHashTagRepository postHashTagRepository,HashTagRepository hashTagRepository) {
 
-    return new TestDataInit(memberRepository, postRepository);
+    return new TestDataInit(memberRepository, postRepository,postHashTagRepository,hashTagRepository);
   }
 
   @RequiredArgsConstructor
@@ -39,6 +45,8 @@ public class FalsylogBackendApplication {
 
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
+    private final PostHashTagRepository postHashTagRepository;
+    private final HashTagRepository hashTagRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void initData() {
@@ -47,9 +55,19 @@ public class FalsylogBackendApplication {
           "1q2w3e4r",
           LocalDateTime.now());
 
+      var hashTag1 = HashTag.builder()
+              .name("AWS")
+              .build();
+
+      var hashTag2 = HashTag.builder()
+              .name("Git")
+              .build();
+
       // TODO: Reactでの無限スクロールのテストの為のデータを入れておく
       var list = new ArrayList<Post>();
+      var postHashTagList = new ArrayList<PostHashTag>();
       for (int i = 0; i < 100; i++) {
+
         var post = Post.builder().title("테스트용 제목입니다. " + i).content(
                 "테스트용 내용입니다. 반적으로 새로운 소프트웨어 개발팀이 구성되면 팀이 코드를 정확히 어떻게 작성할 것인지에 대한 논의가 이루어집니다. 프로그래밍 언어나 주요 프레임워크의 메인테이너가 이에 대해 의견을 가지고 있다면 이러한 논의의 기본적인 출발점을 제공하는 데 도움이 될 수 있습니다.\n"
                     + "\n"
@@ -60,11 +78,29 @@ public class FalsylogBackendApplication {
                     + "이러한 권장 사항 중 일부는 중요하지 않은 코드 스타일에 대한 의견처럼 느껴질 수도 있습니다. 하지만 리액트 문서에서 설명하는 것처럼 권장 사항을 따르지 않는 것은 비용이나 위험이 따릅니다. 여러분은 자유롭게 결정할 수 있지만, 리액트 코어팀은 여러분이나 저보다 훨씬 더 많은 것을 생각했을 것임을 명심하세요. 물론 이것이 항상 옳다는 것을 의미하지는 않지만, 적어도 그들의 의견을 들어보는 것은 그만한 가치가 있을 것입니다.\n"
                     + "\n")
             .build();
+
+        //해시태그 추가
+        PostHashTag postHashTag1 = PostHashTag.builder()
+                .post(post)
+                .hashTag(hashTag1)
+                .build();
+        PostHashTag postHashTag2 = PostHashTag.builder()
+                .post(post)
+                .hashTag(hashTag2)
+                .build();
+        postHashTagList.add(postHashTag1);
+        postHashTagList.add(postHashTag2);
+
+
         post.addMember(member);
+        post.addPostHashTags(Arrays.asList(postHashTag1,postHashTag2));
         list.add(post);
       }
       postRepository.saveAll(list);
+      postHashTagRepository.saveAll(postHashTagList);
       memberRepository.save(member);
+      hashTagRepository.save(hashTag1);
+      hashTagRepository.save(hashTag2);
 
     }
   }
--- src/main/java/jp/falsystack/falsylog_backend/repository/post/PostRepositoryImpl.java
+++ src/main/java/jp/falsystack/falsylog_backend/repository/post/PostRepositoryImpl.java
@@ -3,6 +3,7 @@ package jp.falsystack.falsylog_backend.repository.post;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jp.falsystack.falsylog_backend.domain.Post;
+import jp.falsystack.falsylog_backend.domain.QHashTag;
 import jp.falsystack.falsylog_backend.domain.QPost;
 import jp.falsystack.falsylog_backend.domain.QPostHashTag;
 import jp.falsystack.falsylog_backend.request.post.PostSearch;
@@ -10,8 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static com.querydsl.jpa.JPAExpressions.select;
 
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepositoryCustom {
@@ -22,26 +26,17 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
     public List<Post> getList(PostSearch postSearch) {
 
         BooleanBuilder builder = getBuilder(postSearch);
+        List<Post> posts = new ArrayList<>();
+            posts = query.selectFrom(QPost.post)
+                    .leftJoin(QPost.post.postHashTags, QPostHashTag.postHashTag).fetchJoin()
+                    .leftJoin(QPostHashTag.postHashTag.hashTag).fetchJoin()
+                    .where(builder)
+                    .limit(postSearch.getSize())
+                    .offset(postSearch.getOffset())
+                    .orderBy(QPost.post.id.desc())
+                    .fetch();
 
-        List<Post> posts = query.selectFrom(QPost.post)
-                .leftJoin(QPost.post.postHashTags, QPostHashTag.postHashTag).fetchJoin()
-                .leftJoin(QPostHashTag.postHashTag.hashTag).fetchJoin()
-                .where(builder)
-                .limit(postSearch.getSize())
-                .offset(postSearch.getOffset())
-                .orderBy(QPost.post.id.desc())
-                .fetch();
-
-        // TODO: あとでqueryDSLの勉強が終わったらリファクタリングする。
-        if (StringUtils.hasText(postSearch.getHashTags())) {
-            String[] tags = postSearch.getHashTags().split(",");
-
-            return posts.stream()
-                    .filter(post -> post.getPostHashTags()
-                            .stream()
-                            .anyMatch(postHashTag -> Arrays.asList(tags).contains(postHashTag.getHashTag().getName().substring(1))))
-                    .toList();
-        }
+            
         return posts;
     }
 
@@ -65,10 +60,18 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
             builder.and(QPost.post.createdDateTime.loe(postSearch.getEndDate().plusDays(1).atStartOfDay()));
         }
 
-
         if (StringUtils.hasText(postSearch.getTitle())) {
             builder.and(QPost.post.title.contains(postSearch.getTitle()));
         }
+        if(StringUtils.hasText(postSearch.getHashTags())){
+            builder.and(QPost.post.id.in(
+                    select(QPost.post.id)
+                            .innerJoin(QPost.post.postHashTags, QPostHashTag.postHashTag)
+                            .innerJoin(QPostHashTag.postHashTag.hashTag)
+                            .where(QPostHashTag.postHashTag.hashTag.name.in(postSearch.getHashTags().split(",")))
+            ));
+        }
+
         return builder;
     }
 }
